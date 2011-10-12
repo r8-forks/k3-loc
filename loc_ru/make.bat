6527f031c25729f657c7ef20d7f792f60bad88ea
Alexey.Kharlanov@gmail.com <Alexey.Kharlanov@gmail.com@9baa082a-4cd1-924a-9e80-6435abd1ce71>
2011-10-12 11:25:33 +0000

    [ru] Version 0.15 [ru] Added support for KDX with 3.1 firmware [ru] Changed version in Settings page and added link to web page

--- loc_ru/make.bat
+++ loc_ru/make.bat
@@ -1,5 +1,5 @@
 set HACKNAME=loc_ru
-set PKGVER=0.14
+set PKGVER=0.15
 
 rem copy ..\..\K3Translator.jar .\
 
@@ -21,6 +21,18 @@ set DEVICE=k3g
 ..\bin\kindle_update_tool.py m --%DEVICE% --sign %HACKNAME%_%PKGVER%_%DEVICE%_uninstall uninstall.sh
 
 set DEVICE=k3gb
+..\bin\kindle_update_tool.py m --%DEVICE% --sign %HACKNAME%_%PKGVER%_%DEVICE%_install install.sh bcel-5.2.jar K3Translator.jar translation.jar msp_prefs loc-init loc-bind ui_loc.tar.gz
+..\bin\kindle_update_tool.py m --%DEVICE% --sign %HACKNAME%_%PKGVER%_%DEVICE%_uninstall uninstall.sh
+
+set DEVICE=dxg
+..\bin\kindle_update_tool.py m --%DEVICE% --sign %HACKNAME%_%PKGVER%_%DEVICE%_install install.sh bcel-5.2.jar K3Translator.jar translation.jar msp_prefs loc-init loc-bind ui_loc.tar.gz
+..\bin\kindle_update_tool.py m --%DEVICE% --sign %HACKNAME%_%PKGVER%_%DEVICE%_uninstall uninstall.sh
+
+set DEVICE=dxi
+..\bin\kindle_update_tool.py m --%DEVICE% --sign %HACKNAME%_%PKGVER%_%DEVICE%_install install.sh bcel-5.2.jar K3Translator.jar translation.jar msp_prefs loc-init loc-bind ui_loc.tar.gz
+..\bin\kindle_update_tool.py m --%DEVICE% --sign %HACKNAME%_%PKGVER%_%DEVICE%_uninstall uninstall.sh
+
+set DEVICE=dx
 ..\bin\kindle_update_tool.py m --%DEVICE% --sign %HACKNAME%_%PKGVER%_%DEVICE%_install install.sh bcel-5.2.jar K3Translator.jar translation.jar msp_prefs loc-init loc-bind ui_loc.tar.gz
 ..\bin\kindle_update_tool.py m --%DEVICE% --sign %HACKNAME%_%PKGVER%_%DEVICE%_uninstall uninstall.sh
 
