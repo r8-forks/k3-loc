9963ca57b6006589b13636c5d4e428380f0ca2b5
Alexey.Kharlanov@gmail.com <Alexey.Kharlanov@gmail.com@9baa082a-4cd1-924a-9e80-6435abd1ce71>
2011-12-13 10:46:04 +0000

    K4 Russian localization first preview

---
+++ branches/k4-loc/loc_ru/make.bat
@@ -0,0 +1,31 @@
+set HACKNAME=ru_RU
+set PKGVER=4.1
+
+copy ..\..\K3Translator.jar .\
+
+echo Pack img images
+..\bin\7z a -r img.tar  img\*.gif
+..\bin\7z a img.tar.gz img.tar
+del img.tar
+
+echo Pack low_level_screens images
+..\bin\7z a -r low_level_screens.tar low_level_screens\*.png
+..\bin\7z a low_level_screens.tar.gz low_level_screens.tar
+del low_level_screens.tar
+
+echo Compile translation.jar
+java -cp bcel-5.2.jar;K3Translator.jar Translator mt ./strings translation.jar
+
+echo Create update files
+set DEVICE=k4w
+..\bin\kindle_update_tool.py m --%DEVICE% --sign %HACKNAME%_%PKGVER%_%DEVICE%_install install.sh bcel-5.2.jar K3Translator.jar translation.jar ru.properties low_level_screens.tar.gz img.tar.gz
+rem ..\bin\kindle_update_tool.py m --%DEVICE% --sign %HACKNAME%_%PKGVER%_%DEVICE%_uninstall uninstall.sh
+
+echo Create update archive
+..\bin\7z a update_%HACKNAME%_%PKGVER%.zip *.bin *.keyb
+
+echo Clean up
+del *.bin
+del translation.jar
+del low_level_screens.tar.gz 
+del img.tar.gz
